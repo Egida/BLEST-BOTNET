3d3b2a4365b6ce35dc95c2c2ce2d270ef0a3c643
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-05-23 12:13:49 -0400

    Add files via upload

--- update/blest_update.sh
+++ update/blest_update.sh
@@ -9,8 +9,8 @@ if [ $LAST_COMMIT != $LAST_UPDATE ]; then
         rm -rf /usr/share/BLEST
         rm -rf /usr/bin/blest
         git clone https://github.com/G00Dway/BLEST-BOTNET /usr/share/BLEST > /dev/null 2>&1
-        chmod +x /usr/share/BLEST/core/bin/blest
-        cp -r /usr/share/BLEST/core/bin/blest /usr/bin
+        chmod +x /usr/share/BLEST/core/bin/blest > /dev/null 2>&1
+        cp -r /usr/share/BLEST/core/bin/blest /usr/bin > /dev/null 2>&1
         echo -e "\033[1;32m[+] \033[0mBitirmek için lütfen blest'i kapatıb yeniden açın"
 else
         echo -e "\033[1;77m[i] \033[0mYenilenme Yok"
