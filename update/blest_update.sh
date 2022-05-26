4582d374b875a3379c6023133ee646d68419e7b6
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-05-26 14:22:14 -0400

    Update blest_update.sh

--- update/blest_update.sh
+++ update/blest_update.sh
@@ -6,6 +6,7 @@ LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1
 
 git remote update > /dev/null 2>&1
 if [ $LAST_COMMIT != $LAST_UPDATE ]; then
+        cd
         echo -e "\033[1;33m[!] \033[0mBLEST Yenileniyor, Interneti açık tutun ve ekranı kapatmayın..."
         python3 /usr/share/BLEST/core/setup/git_setup.py /usr/share/BLEST update
         echo -e "\033[1;32m[+] \033[0mBitirmek için lütfen blest'i kapatıb yeniden açın"
